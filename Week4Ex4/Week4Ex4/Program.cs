108069fa76c1c4681525323464ab926077b243bf
James Laptop <james@coopernets.co.uk>
2013-10-21 13:52:40 +0100

    Finished

--- Week4Ex4/Week4Ex4/Program.cs
+++ Week4Ex4/Week4Ex4/Program.cs
@@ -10,6 +10,25 @@ namespace Week4Ex4
     {
         static void Main(string[] args)
         {
+            double width;
+            double height;
+            double length;
+            double volume;
+            Console.WriteLine("Enter Height: ");
+            height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Length: ");
+            length = double.Parse(Console.ReadLine());
+            volume = VolumeCalc(length, width, height);
+            Console.WriteLine("Volume = "+volume.ToString());
+            Console.ReadLine();
+        }
+        static double VolumeCalc(double width,double height, double length)
+        {
+            double result;
+            result = width * height * length;
+            return result;
         }
     }
 }
